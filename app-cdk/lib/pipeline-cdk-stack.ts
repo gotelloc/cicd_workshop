3edf54e5d1800fe134732ff99199d0487c1b6c3e
gotelloc <gotelloc@gmail.com>
2024-08-03 20:17:42 -0400

    Added Docker build and push

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -4,9 +4,17 @@ import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
 import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+
+interface ConsumerProps extends StackProps {
+  ecrRepository: ecr.Repository,
+}
+
 
 export class PipelineCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props: StackProps) {
+  constructor(scope: Construct, id: string, props: ConsumerProps) {
     super(scope, id, props);
 
     const githubToken = secretsmanager.Secret.fromSecretNameV2(this, 'GitHubToken', 'github/personal_access_token2');
@@ -25,8 +33,44 @@ export class PipelineCdkStack extends Stack {
       buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_test.yml'),
     });
 
+    const dockerBuild = new codebuild.PipelineProject(this, 'DockerBuild', {
+      environmentVariables: {
+        IMAGE_TAG: { value: 'latest' },
+        IMAGE_REPO_URI: { value: props.ecrRepository.repositoryUri },
+        AWS_DEFAULT_REGION: { value: process.env.CDK_DEFAULT_REGION },
+      },
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+        privileged: true,
+        computeType: codebuild.ComputeType.LARGE,
+      },
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_docker.yml'),
+    });
+
+    const dockerBuildRolePolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: [
+        'ecr:GetAuthorizationToken',
+        'ecr:BatchCheckLayerAvailability',
+        'ecr:GetDownloadUrlForLayer',
+        'ecr:GetRepositoryPolicy',
+        'ecr:DescribeRepositories',
+        'ecr:ListImages',
+        'ecr:DescribeImages',
+        'ecr:BatchGetImage',
+        'ecr:InitiateLayerUpload',
+        'ecr:UploadLayerPart',
+        'ecr:CompleteLayerUpload',
+        'ecr:PutImage',
+      ],
+    });
+
+    dockerBuild.addToRolePolicy(dockerBuildRolePolicy);
+
     const sourceOutput = new codepipeline.Artifact();
     const unitTestOutput = new codepipeline.Artifact();
+    const dockerBuildOutput = new codepipeline.Artifact();
 
     pipeline.addStage({
       stageName: 'Source',
@@ -50,6 +94,18 @@ export class PipelineCdkStack extends Stack {
           project: codeBuild,
           input: sourceOutput,
           outputs: [unitTestOutput],
+        }),
+      ],
+    });
+    
+    pipeline.addStage({
+      stageName: 'Docker-Push-ECR',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'Docker-Build',
+          project: dockerBuild,
+          input: sourceOutput,
+          outputs: [dockerBuildOutput],
         }),
       ],
     });
