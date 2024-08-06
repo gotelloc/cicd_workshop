ead2c32a47e8838ffc829be30eaab5b36545785a
gotelloc <gotelloc@gmail.com>
2024-08-06 18:48:30 -0400

    Added Blue/Green Prod delivery

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -19,5 +19,8 @@ const prodCdkStack = new AppCdkStack(app, 'prod', {
 const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', {
   ecrRepository: ecrCdkStack.repository,
   fargateServiceTest: testCdkStack.fargateService,
-  fargateServiceProd: prodCdkStack.fargateService,
+  greenTargetGroup: prodCdkStack.greenTargetGroup,
+  greenLoadBalancerListener: prodCdkStack.greenLoadBalancerListener,
+  fargateServiceProd: prodCdkStack.fargateService
 });
+
